2e936a5e234d807d005424702adff79530912274
Ivan Grynenko <grynenko.ivan@gmail.com>
2021-07-12 23:51:09 +0300

    Page

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const topOffset = 120;
+const phone = '+380965235134';
+
+function moveTo(name) {
+    const element = document.getElementById(name);
+    const top = element.offsetTop - topOffset;
+
+    if (top > 0)
+        window.scrollTo(0, top);
+}
+
+function dial() {
+    const element = document.createElement('a');
+    element.setAttribute('href', `tel:${phone}`);
+    element.click();
+}
